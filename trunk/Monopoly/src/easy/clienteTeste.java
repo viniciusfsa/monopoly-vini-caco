e9e3ee34767934006b80833f1a7a094ea43349a9
amarquesferraz <amarquesferraz@5db3b8a8-d2b2-11de-97bf-6350044336de>
2009-11-23 23:27:31 +0000

    Formatação

--- trunk/Monopoly/src/easy/clienteTeste.java
+++ trunk/Monopoly/src/easy/clienteTeste.java
@@ -1,44 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package easy;
-
 /**
  *
  * @author Marcus
  */
+
+package easy;
+
 
 import easyaccept.EasyAcceptFacade;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Marcus
- */
 public class clienteTeste {
 
-     public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-              List<String> files = new ArrayList<String>();
-              //Put the us1.txt file into the "test scripts" list
-              files.add("us1.txt");
-  //Instantiate the Monopoly Game façade
-              facade monopolyGameFacade = new facade();
-              //Instantiate EasyAccept façade
-              EasyAcceptFacade eaFacade = new EasyAcceptFacade(monopolyGameFacade, files);
-  //Execute the tests
-              eaFacade.executeTests();
-  //Print the tests execution results
-              System.out.println(eaFacade.getCompleteResults());
-        }
+        List<String> files = new ArrayList<String>();
+        //Put the us1.txt file into the "test scripts" list
+        files.add("us1.txt");
+        //Instantiate the Monopoly Game façade
+        facade monopolyGameFacade = new facade();
+        //Instantiate EasyAccept façade
+        EasyAcceptFacade eaFacade = new EasyAcceptFacade(monopolyGameFacade, files);
+        //Execute the tests
+        eaFacade.executeTests();
+        //Print the tests execution results
+        System.out.println(eaFacade.getCompleteResults());
+    }
 }
