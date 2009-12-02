abf3d1f3643415dd19f0e2abb97b56fb100c1394
viniciusfsa <viniciusfsa@5db3b8a8-d2b2-11de-97bf-6350044336de>
2009-12-02 04:02:25 +0000

    sem cedilhas

--- trunk/Monopoly/src/Monopoly/Tabuleiro.java
+++ trunk/Monopoly/src/Monopoly/Tabuleiro.java
@@ -61,47 +61,47 @@ public class Tabuleiro extends ArrayList<Lugar>{
                                         "Go"};
 
 
-    private int[] preços_lugares_aluguel = {2,0,4,0,0,6,0,6,8,0,
+    private int[] precos_lugares_aluguel = {2,0,4,0,0,6,0,6,8,0,
                                             10,0,10,12,0,14,0,14,16,0,
                                             18,0,18,20,0,22,22,0,24,0,
                                             26,26,0,28,0,0,35,0,50,0};
 
-    private int[] preços_lugares_compra = {60,0,60,0,200,100,0,100,120,0,
+    private int[] precos_lugares_compra = {60,0,60,0,200,100,0,100,120,0,
                                            140,150,140,160,200,180,0,180,200,0,
                                            220,0,220,240,200,260,260,150,280,0,
                                            300,300,0,320,200,0,350,0,400,0};
   
-    private int[] preços_lugares_aluguel_1_casa = {10,0,20,0,0,30,0,30,40,0,
+    private int[] precos_lugares_aluguel_1_casa = {10,0,20,0,0,30,0,30,40,0,
                                                    50,0,50,60,0,70,0,70,80,0,
                                                    90,0,90,100,0,110,110,0,120,0,
                                                    130,110,0,150,0,0,175,0,200,0};
     
-    private int[] preços_lugares_aluguel_2_casas = {30,0,60,0,0,90,0,90,100,0,
+    private int[] precos_lugares_aluguel_2_casas = {30,0,60,0,0,90,0,90,100,0,
                                                     150,0,150,180,0,200,0,200,220,0,
                                                     250,0,250,300,0,330,330,0,360,0,
                                                     390,330,0,450,0,0,500,0,600,0};
 
-    private int[] preços_lugares_aluguel_3_casas = {90,0,180,0,0,270,0,270,300,0,
+    private int[] precos_lugares_aluguel_3_casas = {90,0,180,0,0,270,0,270,300,0,
                                                     450,0,450,500,0,550,0,550,600,0,
                                                     700,0,700,750,0,800,800,0,850,0,
                                                     900,800,0,100,0,0,1100,0,1400,0};
 
-    private int[] preços_lugares_aluguel_4_casas = {160,0,320,0,0,400,0,400,450,0,
+    private int[] precos_lugares_aluguel_4_casas = {160,0,320,0,0,400,0,400,450,0,
                                                     625,0,625,700,0,750,0,750,800,0,
                                                     875,0,875,925,0,975,975,0,1025,0,
                                                     1100,975,0,1200,0,0,1300,0,1700,0};
         
-    private int[] preços_lugares_aluguel_Hotel =  {250,0,450,0,0,550,0,550,600,0,
+    private int[] precos_lugares_aluguel_Hotel =  {250,0,450,0,0,550,0,550,600,0,
                                                    750,0,750,900,0,950,0,950,1000,0,
                                                    1050,0,1050,1100,0,1150,1150,0,1200,0,
                                                    1275,1150,0,1400,0,0,1500,0,2000,0};
 
-    private int[] preços_lugares_aluguel_Hipoteca = {30,0,30,0,100,50,0,50,60,0,
+    private int[] precos_lugares_aluguel_Hipoteca = {30,0,30,0,100,50,0,50,60,0,
                                                      70,75,70,80,100,90,0,90,100,0,
                                                      110,0,110,120,100,130,130,75,140,0,
                                                      150,130,0,160,100,0,175,0,200,0};
     
-    private int[] preços_da_casa = {50,0,50,0,0,50,0,50,50,50,0,
+    private int[] precos_da_casa = {50,0,50,0,0,50,0,50,50,50,0,
                                   100,0,100,100,0,100,0,100,100,0,
                                   150,0,150,150,0,150,150,0,150,0,
                                   200,150,0,200,0,0,200,0,200,0};
@@ -120,7 +120,7 @@ public class Tabuleiro extends ArrayList<Lugar>{
     public Tabuleiro(){
 
         for (int i = 0; i<this.nomes_lugares.length; i++){
-            this.add(new Lugar(i,this.nomes_lugares[i], this.preços_lugares_compra[i]));
+            this.add(new Lugar(i,this.nomes_lugares[i], this.precos_lugares_compra[i]));
         }
 
     }
@@ -157,7 +157,7 @@ public class Tabuleiro extends ArrayList<Lugar>{
             throw new Exception("Place doesn't exist");
        
         else{
-            int preco = preços_lugares_aluguel[placeId-1];
+            int preco = precos_lugares_aluguel[placeId-1];
             if(preco==0)
                 throw new Exception("This place doesn't have a rent");
             else
@@ -171,7 +171,7 @@ public class Tabuleiro extends ArrayList<Lugar>{
         if(placeId<1 ||placeId>40)
             throw new Exception("Place doesn't exist");
         else{
-            int preco = preços_lugares_compra[placeId-1];
+            int preco = precos_lugares_compra[placeId-1];
             if(preco==0)
                 throw new Exception("This place can't be sold");
             else
