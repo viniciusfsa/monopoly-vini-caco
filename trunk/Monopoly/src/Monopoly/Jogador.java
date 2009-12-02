20fce16ba94be6da074222c5f2b1718e9d1a71ed
amarquesferraz <amarquesferraz@5db3b8a8-d2b2-11de-97bf-6350044336de>
2009-12-02 04:10:20 +0000

    Javadoc

--- trunk/Monopoly/src/Monopoly/Jogador.java
+++ trunk/Monopoly/src/Monopoly/Jogador.java
@@ -2,80 +2,129 @@ package Monopoly;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
+ * Contem as informacoes do Jogador
  * @author Marcus
  */
 public class Jogador {
 
+    /**
+     * id do jogador
+     */
     private int id = 0;
 
+    /**
+     * Nome do jogador
+     */
+    private String Nome = "";
+
+    /**
+     * cor do peao do jogador
+     */
+    private String CorPeao = "";
+
+    /**
+     * dinheiro inicial do jogador
+     */
+    private int dinheiro=1500;
+
+    /**
+     * Lista das posses do jogador
+     */
+    private ArrayList<String> propriedades = new ArrayList<String>();
+
+    /**
+     * Retorna o id do jogador
+     * @return o id
+     */
     public int getId() {
         return id;
     }
 
 
+    /**
+     * Adiciona dinheiro aa conta do jogador
+     * @param dinheiro a ser adicionado
+     */
     public void addDinheiro(int dinheiro){
         this.dinheiro+=dinheiro;
     }
 
+
+    /**
+     * Retira dinheiro da conta do jogador
+     * @param dinheiro dinheiro a ser retirado
+     */
     public void retirarDinheiro(int dinheiro){
         this.addDinheiro(-dinheiro);
     }
 
+
+    /**
+     * Define um id para o jogador
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
-    private String Nome = "";
-    private String CorPeao = "";
-    private int dinheiro=1500;
-    
-    private ArrayList<String> propriedades = new ArrayList<String>();
 
-    public static void main(String[] args) {
-        Jogador j = new Jogador("maria", "preto", 1);
-        j.addPropriedade("casa boa");
-        j.addPropriedade("casa boa2");
 
-        ArrayList a = j.getPropriedades();
-        Iterator i = a.iterator();
-        while (i.hasNext())
-            System.out.println(i.next());
 
-    }
-
+    /**
+     * Instancia um novo jogador
+     * @param Nome nome do jogador
+     * @param CorPeao cor do peao
+     * @param id id
+     */
     public Jogador(String Nome, String CorPeao, int id) {
         this(Nome);
         this.setCorPeao(CorPeao);
         this.id = id;
     }
 
+    /**
+     * Instancia um novo jogador
+     * @param Nome nome do jogador
+     */
     public Jogador (String Nome){
         this.Nome = Nome;
     }
 
     
 
-
+    /**
+     * Obtem a cor do peao do jogador
+     * @return a cor do peao
+     */
     public String getCorPeao() {
         return CorPeao;
     }
 
+
+    /**
+     * Obtem o dinheiro atual do jogador
+     * @return dinheiro
+     */
     public int getDinheiro() {
         return dinheiro;
     }
 
+
+    /**
+     * Seta um determinado valor em dinheiro para o jogador
+     * @param dinheiro nova quantidade de dinheiro a ser definida
+     */
     public void setDinheiro(int dinheiro) {
         this.dinheiro = dinheiro;
     }
 
+
+    /**
+     * Define a cor do peao do jogador
+     * @param CorPeao a nova cor do peao
+     */
     public void setCorPeao(String CorPeao) {
         this.CorPeao = CorPeao;
     }
