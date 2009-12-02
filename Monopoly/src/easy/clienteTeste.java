48d9d86dedbc945a122cf29b43d546981b6e4f11
amarquesferraz <amarquesferraz@5db3b8a8-d2b2-11de-97bf-6350044336de>
2009-12-02 01:15:13 +0000

    Avanços progressivos da US4

--- Monopoly/src/easy/clienteTeste.java
+++ Monopoly/src/easy/clienteTeste.java
@@ -16,9 +16,9 @@ public class clienteTeste {
 
         List<String> files = new ArrayList<String>();
         //Put the us1.txt file into the "test scripts" list
-        files.add("us1.txt");
-        files.add("us2.txt");
-        files.add("us3.txt");
+//        files.add("us1.txt");
+//        files.add("us2.txt");
+//        files.add("us3.txt");
         files.add("us4.txt");
         //Instantiate the Monopoly Game façade
         facade monopolyGameFacade = new facade();
